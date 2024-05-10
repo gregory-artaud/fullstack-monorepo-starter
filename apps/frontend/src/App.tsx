7343d71d6914406eedadde582ad9118e6bd6169a
Gregory ARTAUD <gregory.rtd@gmail.com>
2024-05-10 15:53:39 +0200

    feat: compiste deploy action

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -20,7 +20,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {isEven(count) ? 'even' : 'odd'}
+          count is {isEven(count) ? 'pair' : 'odd'}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
